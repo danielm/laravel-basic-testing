4d33fcc9df3ca0ea73de6878e5dafd11261d6122
Daniel Morales <daniel85mg@gmail.com>
2021-03-04 22:34:52 -0300

    Creat and destoy tags

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Tag;
+use App\Http\Requests\TagRequest;
 
 class TagController extends Controller
 {
@@ -13,5 +14,19 @@ class TagController extends Controller
         return view('index', [
             'tags' => Tag::all()
         ]);
+    }
+
+    public function store(TagRequest $request)
+    {
+        Tag::create($request->validated());
+
+        return redirect(route('tags.index'));
+    }
+
+    public function destroy(Tag $tag)
+    {
+        $tag->delete();
+
+        return redirect(route('tags.index'));
     }
 }
--- tests/Feature/Http/Controllers/TagControllerTest.php
+++ tests/Feature/Http/Controllers/TagControllerTest.php
@@ -10,13 +10,13 @@ use App\Models\Tag;
 
 class TagControllerTest extends TestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase, WithFaker;
 
     public function test_empty_index()
     {
         //$this->withoutExceptionHandling();
 
-        $response = $this->get('/tags')
+        $response = $this->get(route('tags.index'))
             ->assertStatus(200)
             ->assertSee('No tags found');
     }
@@ -27,9 +27,39 @@ class TagControllerTest extends TestCase
 
         //$this->withoutExceptionHandling();
 
-        $response = $this->get('/tags')
+        $response = $this->get(route('tags.index'))
             ->assertStatus(200)
             ->assertSee($tag->name)
             ->assertDontSee('No tags found');
+    }
+
+    public function test_store()
+    {
+        //$this->withoutExceptionHandling();
+
+        $tmp_tag_name = $this->faker->word;
+
+        $this->post(route('tags.store'), ['name' => $tmp_tag_name])
+            ->assertRedirect(route('tags.index'));
+
+        $this->assertDatabaseHas('tags', ['name' => $tmp_tag_name]);
+    }
+
+    public function test_destroy()
+    {
+        $tag = Tag::factory()->create();
+
+        $this->delete(route('tags.destroy', ['tag' => $tag]))
+            ->assertRedirect(route('tags.index'));
+
+        $this->assertDatabaseMissing('tags', ['name' => $tag->name]);
+    }
+
+    public function test_validation()
+    {
+        //$this->withoutExceptionHandling();
+
+        $this->post(route('tags.store'), ['name' => ''])
+            ->assertSessionHasErrors('name');
     }
 }
